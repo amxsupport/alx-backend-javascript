0fd332a0a94890c6812e1554b2867bdd430201a2
amxsupport <itmasterdesigne@gmail.com>
2024-04-15 17:53:41 +0000

    update 10-update_uniq_items.js

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,10 +1,8 @@
 export default function updateUniqueItems(map) {
-    // Check if the argument is a map
     if (!(map instanceof Map)) {
         throw new Error('Cannot process');
     }
     map.forEach((quantity, item) => {
-        // If quantity is 1, update it to 100
         if (quantity === 1) {
             map.set(item, 100);
         }
