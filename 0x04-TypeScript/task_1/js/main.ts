bfca28d231b0480fd636c3c2c68a981b78e05f5b
amxsupport <itmasterdesigne@gmail.com>
2024-04-17 18:43:52 +0000

    update main.ts resolve task 1

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -44,4 +44,24 @@ const printTeacher: printTeacherFunction = (
   lastName: string
 ) => `${firstName.substring(0, 1)}. ${lastName}`;
 
+// Examples:
+const B = document.querySelector('body');
+// task 1:
+const SECTION_T1 = document.createElement('SECTION');
+const H2_T1 = document.createElement('H2');
+H2_T1.innerText = 'Task 1';
+SECTION_T1.appendChild(H2_T1);
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+console.log('Task 1:', teacher3);
+const P_T1 = document.createElement('P');
+P_T1.innerHTML = JSON.stringify(teacher3, null, 2);
+SECTION_T1.appendChild(P_T1);
+B.appendChild(SECTION_T1);
+
 
